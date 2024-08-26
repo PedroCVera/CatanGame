230b578a5ca56e5e49563d080548b2eb8503ef69
Pedro Vera <pedrovera@Pedros-MacBook-Pro.local>
2024-08-26 22:23:16 +0100

    more styling

--- game/views.py
+++ game/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required
 def hello_world(request):
 	return render(request, 'game/game.html')
